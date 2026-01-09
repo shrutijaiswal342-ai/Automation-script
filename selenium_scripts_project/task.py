dd08ec03ba0adc98b218ac9762f86a8f70abdded
Shruti Jaiswal <shrutijaiswal342-ai@gmail.com>
2026-01-09 11:54:57 +0530

    updated existing test logic

--- selenium_scripts_project/task.py
+++ selenium_scripts_project/task.py
@@ -17,7 +17,7 @@ assert search_field.is_displayed(), "Search box not visible"
 search_field.send_keys("samsung mobile")
 search_btn = driver.find_element(By.ID, "nav-search-submit-text")
 search_btn.click()
-time.sleep(3)
+time.sleep(4)
 
 # assert  Search result page loaded #
 assert "samsung" in driver.page_source.lower(), "Search results not loaded"
@@ -25,32 +25,25 @@ assert "samsung" in driver.page_source.lower(), "Search results not loaded"
 
 driver.execute_script("window.scrollBy(0, 300);")
 
-#  brand filter  code #
-brand = driver.find_element(By.XPATH, "//span[text()='Samsung']")
-brand.click()
-time.sleep(4)
+# #  brand filter  code #
+# brand = driver.find_element(By.ID, "//span[text()='Samsung']")
+# brand.click()
+# time.sleep(5)
+
 
 # Assertion Storage filter applied   #
-assert brand.is_displayed(), "brand filter not applied"
+# assert brand.is_displayed(), "brand filter not applied"
 
 # Price filter  code #
 price_range = driver.find_element(By.ID, "p_36/dynamic-picker-2")
 price_range.click()
-time.sleep(4)
+time.sleep(5)
 
 
-#  Scroll   code #
+#  Scroll  code #
 driver.execute_script("window.scrollBy(0, 400);")
-time.sleep(3)
+time.sleep(5)
 
-
-# #  discount filter  code #
-# discount = driver.find_element(By.XPATH, "//span[contains(text(),'10%')]")
-# discount.click()
-# time.sleep(4)
-
-# # Assertion  RAM filter applied   #
-# assert discount.is_displayed(), "discount filter not applied"
 
 #  Pagination   code   #
 pagination = driver.find_element(By.CLASS_NAME, "s-pagination-strip")
@@ -73,22 +66,62 @@ assert open_product, "No products found"
 
 # switch only if new tab opened
 driver.switch_to.window(driver.window_handles[1])
-
 time.sleep(4)
 
 # Assert product detail page
 product_title = driver.find_element(By.ID, "productTitle")
 assert product_title.is_displayed(), "Product detail page not opened"
+time.sleep(3)
 
-# Scroll
+
+driver.execute_script("window.scrollBy(0, 300);")
+
+
+# click to full image view code # 
+img_view = driver.find_element(By.ID, "canvasCaption")
+img_view.click()
+time.sleep(3)
+
+tab_vedio_view = driver.find_element(By.ID, "ivVideosTabHeading")
+tab_vedio_view.click()
+time.sleep(6)
+
+
+tab_img_view = driver.find_element(By.ID, 'ivImagesTabHeading')
+tab_img_view.click()
+time.sleep(4)
+
+
+tab_img_display = driver.find_element(By.ID, 'ivImage_2')
+tab_img_display.click()
+time.sleep(4)
+
+
+tab_img_display2 = driver.find_element(By.ID, 'ivImage_4')
+tab_img_display2.click()
+time.sleep(3)
+
+tab_img_display3 = driver.find_element(By.ID, 'ivImage_5')
+tab_img_display3.click()
+time.sleep(3)
+
+
+#  close button code   #
+close_btn = driver.find_element(By.CLASS_NAME, "a-button-close")
+close_btn.click()
+time.sleep(3)
+
+# Scroll  code #
 driver.execute_script("window.scrollBy(0, 400);")
 time.sleep(3)
+
+
 
 # buy now #
 buy_now = driver.find_element(By.ID, "buy-now-button")
 buy_now.click()
 
 assert buy_now.is_displayed(), "buy now button  not clicked"
-time.sleep(5)
+time.sleep(10)
 
 driver.quit()
